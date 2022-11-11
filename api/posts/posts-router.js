3ec8ebc2939f2dedbbaddae507cb02f65b28cb5a
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-11 13:06:05 -0500

    postRouter has REST connected

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,7 +1,33 @@
 // implement your posts router here
 const express = require('express')
 
-const route = express.Router()
+const router = express.Router()
+
+router.get('/', (req,res) => {
+res.json('foo')
+})
+
+router.get('/:id', (req,res) => {
+
+})
+
+router.post('/', (req,res) => {
+
+})
+
+router.put('/:id', (req,res) => {
+
+})
+
+router.delete('/:id', (req,res) => {
+
+})
+
+router.get('/:id/messages', (req,res) => {
+
+})
 
 
-module.exports = route
+
+
+module.exports = router
